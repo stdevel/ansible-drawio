0a77057efe050fdabe4046ece4eef5380cc7ecc1
Christian Stankowic <info@stankowic-development.net>
2020-10-03 14:49:28 +0200

    add unit tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,10 +1,24 @@
-"""Role testing files using testinfra."""
+"""
+Role unit tests
+"""
+import fnmatch
 
 
-def test_hosts_file(host):
-    """Validate /etc/hosts file."""
-    f = host.file("/etc/hosts")
+def test_drawio_package(host):
+    """
+    Ensure that the drawio package is installed
+    """
+    drawio_pkg = host.package("draw.io")
+    drawio_bin = host.file("/usr/bin/drawio")
 
-    assert f.exists
-    assert f.user == "root"
-    assert f.group == "root"
+    assert drawio_pkg.is_installed
+    assert drawio_bin.exists
+
+
+def test_drawio_appimage(host):
+    """
+    Ensure that drawio AppImage is installed
+    """
+    opt_files = host.file("/opt").listdir()
+    images = fnmatch.filter(opt_files, "drawio*.AppImage")
+    assert images
